00a656544b022fd6268b20a1030cc266a15e5f38
Abhinav Agarwal <119868907+Abhinavagarwa@users.noreply.github.com>
2023-09-11 02:29:32 +0000

    c++ first

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"This is Abhinav Agarwal \n Hello world";
+}
---
+++ ifnelse.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=9;
+    int c=2;
+    if(a>c)
+    {
+        cout<<"9 is greater than 2";
+    }
+}
