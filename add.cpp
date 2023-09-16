35135ee9289539a1bce0882b776e719be4635801
Abhinav Agarwal <119868907+Abhinavagarwa@users.noreply.github.com>
2023-09-16 13:22:03 +0000

    addition of 2 nos in oops

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum=0;
+    cout<<"enter the two numbers\n";
+    cin>>a;
+    cout<<"\n";
+    cin>>b;
+    sum=a+b;
+    cout<<"sum="<<sum<<"\n";
+}
