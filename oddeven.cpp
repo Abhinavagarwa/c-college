dfdb2cf189081c5dc069e1775d239c4ce3cadb64
Abhinav Agarwal <119868907+Abhinavagarwa@users.noreply.github.com>
2023-09-16 13:06:19 +0000

    oops oddeven

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int no;
+   cout<<"enter the number \n";
+   cin>>no;
+   if(no%2==0)
+   {
+      cout<<"no "<<no<<" is even\n";
+   }
+   else
+       cout<<"no is odd\n";
+    return 0;
+}
